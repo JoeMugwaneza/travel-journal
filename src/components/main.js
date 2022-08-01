9bbc11e9999142eb5ed3c80e74824819671b357e
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-08-01 04:44:24 +0200

    finished makeup

--- src/components/main.js
+++ src/components/main.js
@@ -3,13 +3,15 @@ import React from 'react';
 export default function MainContent(){
     return(
         <div className='main-container'>
-            <img src={require("../images/karongi.jpg")}/>
-            <img src={require('../images/locator.png')}/>
-            <h3>Rwanda</h3>
-            <span>View on Google Map</span>
-            <h1>Karongi</h1>
-            <span>Date</span>
-            <p>description of the travel</p>
+            <img src={require("../images/karongi.jpg")} className='travel-image'/>
+            <div className='main-top'>
+                <img src={require('../images/locator.png')}/>
+                <h3 className='travel-location'>Karongi, Western</h3>
+                <span>View on Google Map</span>
+            </div>
+            <h1 className='travel-title'>Karongi Lakeside</h1>
+            <h4>Star - End Date </h4>
+            <p className='travel-description'>A modern, private 4-bedroom home with 200 degree views of breathtaking Lake Kivu. An annex offers two additional en suite rooms for separate rental. Perfect for a special occasion, a personal retreat, or multiple families to relax and enjoy together.</p>
         </div>
     )
 }
--- src/components/travelData.js
+++ src/components/travelData.js
@@ -1,35 +1,35 @@
 export default [
     {
         id: 1,
-        title: "title",
-        location: "location",
-        googleMaps: "google map url", 
-        startDate: "start date", 
-        endDate: "end date", 
-        description: 'description', 
-        image: 'image url'
+        title: 'Karongi Lakeside',
+        location: "Karongi, Western",
+        googleMap: "https://karongilakeviewretreat.com/", 
+        startDate: "August 08, 2021", 
+        endDate: "August 15, 2021", 
+        description: 'A modern, private 4-bedroom home with 200 degree views of breathtaking Lake Kivu. An annex offers two additional en suite rooms for separate rental. Perfect for a special occasion, a personal retreat, or multiple families to relax and enjoy together. ', 
+        imageUrl: 'karongi.jpg'
 
     },
     {
         id: 2,
-        title: "title",
-        location: "location",
-        googleMaps: "google map url", 
-        startDate: "start date", 
-        endDate: "end date", 
-        description: 'description', 
-        image: 'image url'
+        title: "Kivu Serena Hotel",
+        location: "Rubavu, Western",
+        googleMap: "https://www.serenahotels.com/lake-kivu", 
+        startDate: "Jan 10, 2022", 
+        endDate: "Jan 17, 2022", 
+        description: 'Lake Kivu Serena Hotel celebrates the shores of this African Great Lake as a destination of astonishing beauty, where pristine beaches and the sparkling waters of Africa’s sixth largest lake come together in breathtaking fashion', 
+        image: 'rubavu.jpg'
 
     },
     {
         id: 3,
-        title: "title",
-        location: "location",
-        googleMaps: "google map url", 
-        startDate: "start date", 
-        endDate: "end date", 
-        description: 'description', 
-        image: 'image url'
+        title: "Bigogwe View",
+        location: "Rubavu, Western",
+        googleMap: "https://www.ibereryabigogwe.com/", 
+        startDate: "May 5, 2022", 
+        endDate: "May 12, 2022", 
+        description: 'Welcome to Ibere rya Bigogwe tourist service, we offer tourist services which revolve around Bigogwe (a very unique place in Nyabihu district) those includes Camping, Hiking, Cow boys sport and many more.', 
+        image: 'vist-rwanda.jpg'
 
     }
 ]
