c0b92703fa30ecad89c7016cc42a2ca409b3e913
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-08-01 06:05:39 +0200

    makeup

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function NavBar(){
     return(
         <div className='nav-bar'>
-            <img src=''/>
-            <h1>My Travel Journal</h1>
+                <img src={require("../images/globe.png")} className="nav-logo"/>
+                <h1>My Travel Journal</h1>
         </div>
     )
 }
