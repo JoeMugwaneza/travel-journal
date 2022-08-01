19d0a2101fef77de63b1e8a549d225fdf53f50f8
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-08-01 04:49:10 +0200

    put the content in two section for easy arrangement

--- src/components/main.js
+++ src/components/main.js
@@ -3,15 +3,20 @@ import React from 'react';
 export default function MainContent(){
     return(
         <div className='main-container'>
+            {/* IMAGE SIDE */}
             <img src={require("../images/karongi.jpg")} className='travel-image'/>
-            <div className='main-top'>
-                <img src={require('../images/locator.png')}/>
-                <h3 className='travel-location'>Karongi, Western</h3>
-                <span>View on Google Map</span>
+
+            {/* CONTENT SIDE */}
+            <div className='travel-content'>
+                <div className='main-top'>
+                    <img src={require('../images/locator.png')}/>
+                    <h3 className='travel-location'>Karongi, Western</h3>
+                    <span>View on Google Map</span>
+                </div>
+                <h1 className='travel-title'>Karongi Lakeside</h1>
+                <h4 className='travel-date'>Star - End Date </h4>
+                <p className='travel-description'>A modern, private 4-bedroom home with 200 degree views of breathtaking Lake Kivu. An annex offers two additional en suite rooms for separate rental. Perfect for a special occasion, a personal retreat, or multiple families to relax and enjoy together.</p>
             </div>
-            <h1 className='travel-title'>Karongi Lakeside</h1>
-            <h4>Star - End Date </h4>
-            <p className='travel-description'>A modern, private 4-bedroom home with 200 degree views of breathtaking Lake Kivu. An annex offers two additional en suite rooms for separate rental. Perfect for a special occasion, a personal retreat, or multiple families to relax and enjoy together.</p>
         </div>
     )
 }
