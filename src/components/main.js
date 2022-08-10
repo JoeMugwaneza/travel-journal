5dde245ceb40777327877d0c315941113875ab66
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-08-10 04:49:51 +0200

    all properties added dynamically

--- src/components/main.js
+++ src/components/main.js
@@ -10,12 +10,13 @@ export default function MainContent(props){
             <div className='travel-content'>
                 <div className='main-top'>
                     <img src={require('../images/locator.png')} className='map-locator'/>
-                    <h3 className='travel-location'>Karongi</h3>
-                    <span className='location-map'>View on Google Map</span>
+                    <h3 className='travel-location'>{props.title}</h3>
+                    <a href={props.googleMap} className='location-map' target="_blank">View on Google Map</a>
+                    <span ></span>
                 </div>
-                <h1 className='travel-title'>Karongi Lakeside</h1>
-                <h4 className='travel-date'>Star - End Date </h4>
-                <p className='travel-description'>A modern, private 4-bedroom home with 200 degree views of breathtaking Lake Kivu. An annex offers two additional en suite rooms for separate rental.</p>
+                <h1 className='travel-title'>{props.location}</h1>
+                <h4 className='travel-date'>{props.startDate} - {props.endDate} </h4>
+                <p className='travel-description'>{props.description}</p>
             </div>
         </div>
     )
